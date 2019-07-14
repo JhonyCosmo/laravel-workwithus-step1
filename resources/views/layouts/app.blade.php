f5b434f8136747ea577f1188b5be6a002fda773b
Vanjhony Cosmo <jhonycosmo@gmail.com>
2019-07-13 23:41:52 -0300

    Alterando informações de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,15 +34,14 @@
                         @guest
                         @else
                             <div class="container" >
-                                <label class="row">Nome:{{ __(Auth::user()->name) }}</label>
-                                <label class="row">E-mail:{{ __(Auth::user()->email) }}</label>
+                                <label class="row" style="font-weight:bold;font-size:12px;">Nome:{{ __(Auth::user()->name) }}</label>
+                                <label class="row" style="font-weight:bold;font-size:12px;">E-mail:{{ __(Auth::user()->email) }}</label>
                             </div>
                         @endguest
 
                      </div>
 
                 </div>
-
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -67,30 +66,22 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                            <a class="nav-link" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Sair') }}
-                                    </a>
+                                {{ __('Sair') }}
+                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
-
-
 
         <main class="py-4">
             @yield('content')
